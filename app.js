afe884c874eb695b4d82eabcbe7a663ecddb82ba
mreshtaiwi <mr.eshtaiwi@gmail.com>
2021-06-24 13:46:10 +0300

    js practice

---
+++ app.js
@@ -0,0 +1,47 @@
+/*
+1- Write a JavaScript program to know if the input is positive  or negative , " 0 considered as positive "?
+*/
+
+
+/*
+2- Write a JavaScript program takes 3 input from the user and print the max on the console?
+*/
+
+
+/*
+3- Write a JavaScript for loop that will iterate from 0 to 20. For each iteration,
+it will check if the current number is odd or even, and display a message on the console?
+
+Sample Output:
+"0 is even"
+"1 is odd"
+"2 is even"
+.
+.
+.
+.
+.
+"19 is odd"
+"20 is even"
+*/
+
+
+/*
+4- Write a JavaScript program to calculate the sum of even numbers from 1-100 ?
+*/
+
+
+/*
+5- Write a JavaScript program to take 2 numbers from the user and print
+ex:-
+num1 = 3
+num2 = 3
+
+the sum is 6
+the multiplication is 9
+*/
+
+
+/*
+6- Write a function that takes an integer minutes and converts it to seconds.
+*/
