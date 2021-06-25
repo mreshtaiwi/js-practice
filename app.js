ac239323f0447dff3fec4478e5690b5275e9736a
Naziha-1986 <nazihaqawasmeh@gmail.com>
2021-06-26 00:08:56 +0300

    q3 solved

--- app.js
+++ app.js
@@ -3,10 +3,34 @@
 */
 
 
+function numerType(l) {
+    let anynumber = l;
+    if (anynumber >= 0) {
+        console.log(anynumber, 'is a positive.');
+    } else {
+        console.log(anynumber, 'is negative.');
+    }
+}
+numerType(10);
+
 /*
 2- Write a JavaScript program takes 3 input from the user and print the max on the console?
 */
 
+let num1 = Number(prompt(' enter 1st number'));
+let num2 = Number(prompt('enter  2nd number'));
+let num3 = Number(prompt('enter 3rd number'));
+function maxNumber(a, b, c) {
+    let message1 = ' is the max number.';
+    if (num1 > num2 && num1 > num3) {
+        console.log(num2, message1);
+    } else if (num2 > num1 && num2 > num3) {
+        console.log(num2, message1);
+    } else {
+        console.log(num3, message1);
+    }
+}
+maxNumber(num1, num2, num3);
 
 /*
 3- Write a JavaScript for loop that will iterate from 0 to 20. For each iteration,
@@ -24,6 +48,20 @@ Sample Output:
 "19 is odd"
 "20 is even"
 */
+
+function numberType() {
+    let messege2 = ' is even.';
+    let messege3 = 'is odd';
+    for (let i = 0; i <= 20; i++) {
+        if (i % 2 == 0) {
+            console.log(i, messege2);
+        } else {
+            console.log(i, messege3);
+        }
+    }
+}
+    numberType();
+
 
 
 /*
