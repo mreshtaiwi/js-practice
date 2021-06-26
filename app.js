78363cdc6067de62101264e5375855e0c5e365cc
duajaradat <duajaradat164@gmail.com>
2021-06-27 01:22:57 +0300

    solved problem4

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 /*
 1- Write a JavaScript program to know if the input is positive  or negative , " 0 considered as positive "?
 */
-// let input=Number(prompt("Please, Enter a number:"));
-// if (input>=0){
-//     console.log("The Number is Positive");
-//     alert("The Number is Positive");
-// }else if (input<0){
-//     console.log("The Number is Negative");
-//     alert("The Number is Negative");
-// }
+let input=Number(prompt("Please, Enter a number:"));
+if (input>=0){
+    console.log("The Number is Positive");
+    alert("The Number is Positive");
+}else if (input<0){
+    console.log("The Number is Negative");
+    alert("The Number is Negative");
+}
 
 
 
 // /*
 // 2- Write a JavaScript program takes 3 input from the user and print the max on the console?
 // */
-// alert("You need to enter three numbers");
-// let numOne=Number(prompt("Please, enter you first number ?"));
-// console.log(numOne);
-// let numTwo=Number(prompt("Please, enter you second number ?"));
-// console.log(numTwo);
-// let numThree=Number(prompt("Please, enter you third number ?"));
-// console.log(numThree);
+alert("You need to enter three numbers");
+let numOne=Number(prompt("Please, enter you first number ?"));
+console.log(numOne);
+let numTwo=Number(prompt("Please, enter you second number ?"));
+console.log(numTwo);
+let numThree=Number(prompt("Please, enter you third number ?"));
+console.log(numThree);
 
-// if (numOne>numTwo && numOne>numThree){
-//     alert("The maximum between the three numbers is "+numOne);
-//     console.log("The maximum between the three numbers is "+numOne);
-// }else if(numTwo>numOne && numTwo>numThree){
-//     alert("The maximum between the three numbers is "+numTwo);
-//     console.log("The maximum between the three numbers is "+numTwo);
-// } else if(numThree>numOne && numThree>numTwo){
-//     alert("The maximum between the three numbers is "+numThree);
-//     console.log("The maximum between the three numbers is "+numThree);
-// }
+if (numOne>numTwo && numOne>numThree){
+    alert("The maximum between the three numbers is "+numOne);
+    console.log("The maximum between the three numbers is "+numOne);
+}else if(numTwo>numOne && numTwo>numThree){
+    alert("The maximum between the three numbers is "+numTwo);
+    console.log("The maximum between the three numbers is "+numTwo);
+} else if(numThree>numOne && numThree>numTwo){
+    alert("The maximum between the three numbers is "+numThree);
+    console.log("The maximum between the three numbers is "+numThree);
+}
 
 
 
@@ -55,13 +55,13 @@ Sample Output:
 "20 is even"`
 */
 
-// for(let i = 0; i <= 20; i++){
-//     if (i%2==0) {
-//         console.log(i + " is even number")
-//     }else{
-//         console.log(i+ " is odd number")
-//     }
-// }
+for(let i = 0; i <= 20; i++){
+    if (i%2==0) {
+        console.log(i + " is even number")
+    }else{
+        console.log(i+ " is odd number")
+    }
+}
 
 
 /*
