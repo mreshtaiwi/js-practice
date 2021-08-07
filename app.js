1393f69f92cd88d9dee2e18e5319aca922dca05c
Qamar <qalkhatib0@gmail.com>
2021-08-08 01:00:58 +0300

    problem2 solved

--- app.js
+++ app.js
@@ -7,7 +7,24 @@
 2- Write a JavaScript program takes 3 input from the user and print the max on the console?
 */
 
+function isMax(){
+let num1 = Number(prompt("Enter first number"));
+let num2 = Number(prompt("Enter second number"));
+let num3 = Number(prompt("Enter Third number"));
 
+if (num1 >= num2 && num1 >= num3){
+    console.log(`${num1} is the max`)
+}
+else if (num2 >= num1 && num2 >= num3){
+    console.log(`${num2} is the max`)
+}
+else{
+    console.log(`${num3} is the max`)
+}
+console.log(`The numbers are ${num1}, ${num2}, ${num3}`)
+
+}
+isMax();
 /*
 3- Write a JavaScript for loop that will iterate from 0 to 20. For each iteration,
 it will check if the current number is odd or even, and display a message on the console?
